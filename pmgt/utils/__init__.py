4668eab8f0aa7c9ca855964230a24ab690840064
Sangwoo Han <realappsdev@gmail.com>
2022-01-07 00:55:55 +0900

    Add utils

---
+++ pmgt/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Created on 2022/01/05
+@author Sangwoo Han
+"""
+from .base import log_elapsed_time, set_seed
+
+__all__ = ["log_elapsed_time", "set_seed"]
---
+++ pmgt/utils/base.py
@@ -0,0 +1,34 @@
+"""
+Created on 2022/01/05
+@author Sangwoo Han
+"""
+import random
+import time
+from datetime import timedelta
+from functools import wraps
+
+import numpy as np
+import torch
+from logzero import logger
+
+
+def log_elapsed_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        end = time.time()
+
+        elapsed = end - start
+        logger.info(f"elapsed time: {end - start:.2f}s, {timedelta(seconds=elapsed)}")
+
+        return ret
+
+    return wrapper
+
+
+def set_seed(seed: int) -> None:
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
