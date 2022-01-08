5550500f5c6a997e2c0346070b079841fcb9d65a
Sangwoo Han <realappsdev@gmail.com>
2022-01-08 22:02:32 +0900

    Update run_hpo_ncf_gmf.sh

--- scripts/run_hpo_ncf_gmf.sh
+++ scripts/run_hpo_ncf_gmf.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# export MLFLOW_TRACKING_URI=http://115.145.135.72:5000
-
-DATASET=VG
-MODEL=GMF
-
 args=(
     --study-name "NCF-GMF"
     --train-config-path './config/train_ncf_params.json'
     --hp-config-path './config/hpo_ncf_gmf_params.yaml'
+    --n-trials 16
     --train-func 'train_ncf'
 )
 
