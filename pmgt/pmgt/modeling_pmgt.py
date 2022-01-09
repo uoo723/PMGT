e2197af36a518a6ca8b9b5848e9361a8e91d835b
Sangwoo Han <realappsdev@gmail.com>
2022-01-10 01:04:02 +0900

    Move class to modeling_pmgt from models

--- pmgt/pmgt/modeling_pmgt.py
+++ pmgt/pmgt/modeling_pmgt.py
@@ -7,11 +7,12 @@ Created on 2022/01/08
 import math
 
 import torch
-import torch.nn as nn
 import torch.linalg
+import torch.nn as nn
 import torch.utils.checkpoint
 from transformers.modeling_outputs import BaseModelOutput
 from transformers.modeling_utils import (
+    PreTrainedModel,
     apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
     prune_linear_layer,
@@ -21,6 +22,91 @@ from transformers.models.bert.modeling_bert import (
     BertOutput,
     BertSelfOutput,
 )
+
+from .configuration_pmgt import PMGTConfig
+
+
+class PMGTPretrainedModel(PreTrainedModel):
+    """
+    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
+    models.
+    """
+
+    config_class = PMGTConfig
+    base_model_prefix = "pmgt"
+    supports_gradient_checkpointing = True
+
+    def _init_weights(self, module):
+        """Initialize the weights"""
+        if isinstance(module, nn.Linear):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def _set_gradient_checkpointing(self, module, value=False):
+        if isinstance(module, PMGTEncoder):
+            module.gradient_checkpointing = value
+
+
+class PMGTModel(PMGTPretrainedModel):
+    pass
+    # def __init__(self, config, add_pooling_layer=True):
+    #     super().__init__(config)
+    #     self.config = config
+
+    #     self.embeddings = PMGTEmbeddings(config)
+    #     self.encoder = PMGTEncoder(config)
+    #     self.pooler = BertPooler(config) if add_pooling_layer else None
+
+    #     self.init_weights()
+
+    # def get_input_embeddings(self):
+    #     return self.embeddings.raw_feature_embeddings
+
+    # def set_input_embeddings(self, value):
+    #     self.embeddings.raw_feature_embeddings = value
+
+    # def _prune_heads(self, heads_to_prune):
+    #     for layer, heads in heads_to_prune.items():
+    #         self.encoder.layer[layer].attention.prune_heads(heads)
+
+    # def forward(
+    #     self,
+    #     raw_features,
+    #     wl_role_ids,
+    #     init_pos_ids,
+    #     hop_dis_ids,
+    #     head_mask=None,
+    #     residual_h=None,
+    # ):
+    #     if head_mask is None:
+    #         head_mask = [None] * self.config.num_hidden_layers
+
+    #     embedding_output = self.embeddings(
+    #         raw_features=raw_features,
+    #         wl_role_ids=wl_role_ids,
+    #         init_pos_ids=init_pos_ids,
+    #         hop_dis_ids=hop_dis_ids,
+    #     )
+    #     encoder_outputs = self.encoder(
+    #         embedding_output, head_mask=head_mask, residual_h=residual_h
+    #     )
+    #     sequence_output = encoder_outputs[0]
+    #     pooled_output = self.pooler(sequence_output)
+    #     outputs = (
+    #         sequence_output,
+    #         pooled_output,
+    #     ) + encoder_outputs[1:]
+    #     return outputs
 
 
 class PMGTEmbeddings(nn.Module):
