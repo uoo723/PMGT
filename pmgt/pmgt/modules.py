75291e7ca61deff96a302fb2fc1805d19e809ad4
Sangwoo Han <realappsdev@gmail.com>
2022-01-09 23:08:22 +0900

    Implement PMGTEmbeddings

--- pmgt/pmgt/modules.py
+++ pmgt/pmgt/modules.py
@@ -4,16 +4,25 @@ Created on 2022/01/08
 @ref https://github.com/jwzhanggy/Graph-Bert/blob/master/code/MethodBertComp.py
 """
 
+import math
+
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint
 from transformers.configuration_utils import PretrainedConfig
 from transformers.modeling_outputs import BaseModelOutput
+from transformers.modeling_utils import (
+    apply_chunking_to_forward,
+    find_pruneable_heads_and_indices,
+    prune_linear_layer,
+)
 from transformers.models.bert.modeling_bert import (
     BertAttention,
     BertIntermediate,
     BertOutput,
     BertPredictionHeadTransform,
+    BertSelfAttention,
+    BertSelfOutput,
 )
 
 
@@ -25,27 +34,31 @@ class PMGTConfig(PretrainedConfig):
         self,
         node_size=7252,
         hidden_size=128,
-        feat_sizes=[1536, 768],
+        feat_hidden_sizes=[1536, 768],
+        freeze_feat_embeddings=True,
         num_hidden_layers=5,
         num_attention_heads=1,
         intermediate_size=128,
         hidden_act="gelu",
         hidden_dropout_prob=0.1,
         attention_probs_dropout_prob=0.1,
+        max_position_embeddings=100,
         initializer_range=0.02,
         layer_norm_eps=1e-12,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self.node_size = node_size
         self.hidden_size = hidden_size
-        self.feat_sizes = feat_sizes
+        self.feat_hidden_sizes = feat_hidden_sizes
+        self.freeze_feat_embeddings = freeze_feat_embeddings
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
         self.hidden_act = hidden_act
         self.intermediate_size = intermediate_size
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
         self.initializer_range = initializer_range
         self.layer_norm_eps = layer_norm_eps
 
@@ -64,8 +77,6 @@ class PMGTEncoder(nn.Module):
         hidden_states,
         attention_mask=None,
         head_mask=None,
-        encoder_hidden_states=None,
-        encoder_attention_mask=None,
         output_attentions=False,
         output_hidden_states=False,
         return_dict=True,
@@ -92,16 +103,12 @@ class PMGTEncoder(nn.Module):
                     hidden_states,
                     attention_mask,
                     layer_head_mask,
-                    encoder_hidden_states,
-                    encoder_attention_mask,
                 )
             else:
                 layer_outputs = layer_module(
                     hidden_states,
                     attention_mask,
                     layer_head_mask,
-                    encoder_hidden_states,
-                    encoder_attention_mask,
                     output_attentions=output_attentions,
                 )
 
@@ -131,40 +138,65 @@ class PMGTEncoder(nn.Module):
 
 
 class PMGTEmbeddings(nn.Module):
-    """Construct the embeddings from features, wl, position and hop vectors."""
+    def __init__(self, config):
+        super().__init__()
+        self.feat_embeddings = nn.ModuleList(
+            nn.Embedding(config.node_size, feat_hidden_size)
+            for feat_hidden_size in config.feat_hidden_sizes
+        )
+        self.position_embeddings = nn.Embedding(
+            config.max_position_embeddings, config.hidden_size
+        )
+        self.role_embeddings = nn.Embedding(2, config.hidden_size)
 
-    # def __init__(self, config):
-    #     super().__init__()
-    #     self.raw_feature_embeddings = nn.Linear(config.x_size, config.hidden_size)
-    #     self.wl_role_embeddings = nn.Embedding(
-    #         config.max_wl_role_index, config.hidden_size
-    #     )
-    #     self.inti_pos_embeddings = nn.Embedding(
-    #         config.max_inti_pos_index, config.hidden_size
-    #     )
-    #     self.hop_dis_embeddings = nn.Embedding(
-    #         config.max_hop_dis_index, config.hidden_size
-    #     )
+        self.feat_linear = nn.ModuleList(
+            nn.Linear(feat_hidden_size, config.hidden_size)
+            for feat_hidden_size in config.feat_hidden_sizes
+        )
 
-    #     self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-    #     self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        num_feats = len(config.feat_hidden_sizes)
+        self.attention = nn.Sequential(
+            nn.Tanh(),
+            nn.Linear(num_feats * config.hidden_size, num_feats),
+            nn.Softmax(dim=-1),
+        )
 
-    # def forward(
-    #     self, raw_features=None, wl_role_ids=None, init_pos_ids=None, hop_dis_ids=None
-    # ):
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    #     raw_feature_embeds = self.raw_feature_embeddings(raw_features)
-    #     role_embeddings = self.wl_role_embeddings(wl_role_ids)
-    #     position_embeddings = self.inti_pos_embeddings(init_pos_ids)
-    #     hop_embeddings = self.hop_dis_embeddings(hop_dis_ids)
+        if config.freeze_feat_embeddings:
+            for feat_emb in self.feat_embeddings:
+                feat_emb.requires_grad_(False)
 
-    #     # ---- here, we use summation ----
-    #     embeddings = (
-    #         raw_feature_embeds + role_embeddings + position_embeddings + hop_embeddings
-    #     )
-    #     embeddings = self.LayerNorm(embeddings)
-    #     embeddings = self.dropout(embeddings)
-    #     return embeddings
+    def forward(self, node_ids):
+        batch_size, seq_len = node_ids.size()
+
+        feat_embeds = [
+            feat_linear(feat_embeddings(node_ids))
+            for feat_embeddings, feat_linear in zip(
+                self.feat_embeddings, self.feat_linear
+            )
+        ]
+
+        attention_scores = self.attention(torch.cat(feat_embeds, dim=-1))
+        feat_embeds = attention_scores.unsqueeze(-1) * torch.stack(feat_embeds, dim=2)
+        feat_embeds = feat_embeds.sum(dim=2)
+
+        position_ids = torch.arange(
+            seq_len, dtype=torch.long, device=node_ids.device
+        ).repeat(batch_size, 1)
+        position_embeds = self.position_embeddings(position_ids)
+
+        role_ids = torch.ones(seq_len, dtype=torch.long, device=node_ids.device)
+        role_ids[0] = 0
+        role_ids = role_ids.repeat(batch_size, 1)
+        role_embeds = self.role_embeddings(role_ids)
+
+        embeds = feat_embeds + position_embeds + role_embeds
+        embeds = self.LayerNorm(embeds)
+        embeds = self.dropout(embeds)
+
+        return embeds
 
 
 class NodeConstructOutputLayer(nn.Module):
@@ -189,47 +221,214 @@ class NodeConstructOutputLayer(nn.Module):
 
 
 class PMGTLayer(nn.Module):
-    pass
-    # def __init__(self, config):
-    #     super().__init__()
-    #     self.chunk_size_feed_forward = config.chunk_size_feed_forward
-    #     self.seq_len_dim = 1
-    #     self.attention = BertAttention(config)
-    #     self.intermediate = BertIntermediate(config)
-    #     self.output = BertOutput(config)
+    def __init__(self, config):
+        super().__init__()
+        self.chunk_size_feed_forward = config.chunk_size_feed_forward
+        self.seq_len_dim = 1
+        self.attention = PMGTAttention(config)
+        self.intermediate = BertIntermediate(config)
+        self.output = BertOutput(config)
 
-    # def forward(
-    #     self,
-    #     hidden_states,
-    #     attention_mask=None,
-    #     head_mask=None,
-    #     encoder_hidden_states=None,
-    #     encoder_attention_mask=None,
-    #     past_key_value=None,
-    #     output_attentions=False,
-    # ):
-    #     self_attention_outputs = self.attention(
-    #         hidden_states, attention_mask, head_mask
-    #     )
-    #     attention_output = self_attention_outputs[0]
-    #     outputs = self_attention_outputs[
-    #         1:
-    #     ]  # add self attentions if we output attention weights
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        output_attentions=False,
+    ):
+        self_attention_outputs = self.attention(
+            hidden_states,
+            attention_mask,
+            head_mask,
+            output_attentions=output_attentions,
+        )
+        attention_output = self_attention_outputs[0]
+        outputs = self_attention_outputs[1:]
 
-    #     if self.is_decoder and encoder_hidden_states is not None:
-    #         cross_attention_outputs = self.crossattention(
-    #             attention_output,
-    #             attention_mask,
-    #             head_mask,
-    #             encoder_hidden_states,
-    #             encoder_attention_mask,
-    #         )
-    #         attention_output = cross_attention_outputs[0]
-    #         outputs = (
-    #             outputs + cross_attention_outputs[1:]
-    #         )  # add cross attentions if we output attention weights
+        layer_output = apply_chunking_to_forward(
+            self.feed_forward_chunk,
+            self.chunk_size_feed_forward,
+            self.seq_len_dim,
+            attention_output,
+        )
+        outputs = (layer_output,) + outputs
 
-    #     intermediate_output = self.intermediate(attention_output)
-    #     layer_output = self.output(intermediate_output, attention_output)
-    #     outputs = (layer_output,) + outputs
-    #     return outputs
+        return outputs
+
+    def feed_forward_chunk(self, attention_output):
+        intermediate_output = self.intermediate(attention_output)
+        layer_output = self.output(intermediate_output, attention_output)
+        return layer_output
+
+
+class PMGTAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.self = PMGTSelfAttention(config)
+        self.output = BertSelfOutput(config)
+        self.pruned_heads = set()
+
+    def prune_heads(self, heads):
+        if len(heads) == 0:
+            return
+        heads, index = find_pruneable_heads_and_indices(
+            heads,
+            self.self.num_attention_heads,
+            self.self.attention_head_size,
+            self.pruned_heads,
+        )
+
+        # Prune linear layers
+        self.self.query = prune_linear_layer(self.self.query, index)
+        self.self.key = prune_linear_layer(self.self.key, index)
+        self.self.value = prune_linear_layer(self.self.value, index)
+        self.output.dense = prune_linear_layer(self.output.dense, index, dim=1)
+
+        # Update hyper params and store pruned heads
+        self.self.num_attention_heads = self.self.num_attention_heads - len(heads)
+        self.self.all_head_size = (
+            self.self.attention_head_size * self.self.num_attention_heads
+        )
+        self.pruned_heads = self.pruned_heads.union(heads)
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        past_key_value=None,
+        output_attentions=False,
+    ):
+        self_outputs = self.self(
+            hidden_states,
+            attention_mask,
+            head_mask,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+        )
+        attention_output = self.output(self_outputs[0], hidden_states)
+        outputs = (attention_output,) + self_outputs[
+            1:
+        ]  # add attentions if we output them
+        return outputs
+
+
+class PMGTSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(
+            config, "embedding_size"
+        ):
+            raise ValueError(
+                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
+                f"heads ({config.num_attention_heads})"
+            )
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+        self.position_embedding_type = getattr(
+            config, "position_embedding_type", "absolute"
+        )
+        if (
+            self.position_embedding_type == "relative_key"
+            or self.position_embedding_type == "relative_key_query"
+        ):
+            self.max_position_embeddings = config.max_position_embeddings
+            self.distance_embedding = nn.Embedding(
+                2 * config.max_position_embeddings - 1, self.attention_head_size
+            )
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (
+            self.num_attention_heads,
+            self.attention_head_size,
+        )
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        output_attentions=False,
+    ):
+        mixed_query_layer = self.query(hidden_states)
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+
+        if (
+            self.position_embedding_type == "relative_key"
+            or self.position_embedding_type == "relative_key_query"
+        ):
+            seq_length = hidden_states.size()[1]
+            position_ids_l = torch.arange(
+                seq_length, dtype=torch.long, device=hidden_states.device
+            ).view(-1, 1)
+            position_ids_r = torch.arange(
+                seq_length, dtype=torch.long, device=hidden_states.device
+            ).view(1, -1)
+            distance = position_ids_l - position_ids_r
+            positional_embedding = self.distance_embedding(
+                distance + self.max_position_embeddings - 1
+            )
+            positional_embedding = positional_embedding.to(
+                dtype=query_layer.dtype
+            )  # fp16 compatibility
+
+            if self.position_embedding_type == "relative_key":
+                relative_position_scores = torch.einsum(
+                    "bhld,lrd->bhlr", query_layer, positional_embedding
+                )
+                attention_scores = attention_scores + relative_position_scores
+            elif self.position_embedding_type == "relative_key_query":
+                relative_position_scores_query = torch.einsum(
+                    "bhld,lrd->bhlr", query_layer, positional_embedding
+                )
+                relative_position_scores_key = torch.einsum(
+                    "bhrd,lrd->bhlr", key_layer, positional_embedding
+                )
+                attention_scores = (
+                    attention_scores
+                    + relative_position_scores_query
+                    + relative_position_scores_key
+                )
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        if attention_mask is not None:
+            # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        outputs = (
+            (context_layer, attention_probs) if output_attentions else (context_layer,)
+        )
+
+        return outputs
