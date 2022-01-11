d2155ecd96ba997f5d6bcc5d85db87c22ed1c90c
Sangwoo Han <realappsdev@gmail.com>
2022-01-11 19:56:14 +0900

    Create preprocessing subfolder

--- pmgt/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Created on 2022/01/04
-@author Sangwoo Han
-"""
-from .image import AmazonReviewImageDataset
-from .text import AmazonReviewTextDataset, text_collate_fn
-
-__all__ = ["AmazonReviewImageDataset", "AmazonReviewTextDataset", "text_collate_fn"]
---
+++ pmgt/preprocessing/__init__.py
@@ -0,0 +1,4 @@
+"""
+Created on 2022/01/11
+@author Sangwoo Han
+"""
