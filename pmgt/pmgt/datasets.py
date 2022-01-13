6230b754ccb690259b79acee5bf5a9d8db08fca9
Sangwoo Han <realappsdev@gmail.com>
2022-01-13 20:32:54 +0900

    Seperate get_input_tensor to a function rather than class method

--- pmgt/pmgt/datasets.py
+++ pmgt/pmgt/datasets.py
@@ -11,6 +11,74 @@ import scipy.special as ss
 import torch
 
 
+def _sample_context_neigh(
+    graph: nx.Graph,
+    target_node: int,
+    hop_sampling_sizes: List[int],
+    max_num_ctx_neigh: int,
+) -> Tuple[List[int], int]:
+    scores = defaultdict(lambda: 0)
+    depth = len(hop_sampling_sizes)
+
+    sampled_nodes = [[target_node]] + [[] for _ in range(depth)]
+
+    for k, sample_size in enumerate(hop_sampling_sizes, start=1):
+        for node in sampled_nodes[k - 1]:
+            weights = ss.softmax(
+                np.asarray([v["weight"] for v in graph[node].values()])
+            )
+            node_list = np.random.choice(
+                graph[node], size=sample_size, replace=True, p=weights
+            ).tolist()
+            sampled_nodes[k].extend(node_list)
+
+        counter = Counter(sampled_nodes[k])
+
+        for node, freq in counter.items():
+            if node == target_node:
+                continue
+            scores[node] += freq * (depth - k + 1)
+
+    ctx_nodes, _ = zip(*sorted(scores.items(), key=lambda item: item[1], reverse=True))
+
+    ctx_nodes = list(ctx_nodes)
+
+    if len(ctx_nodes) < max_num_ctx_neigh:
+        num_ctx_nodes = len(ctx_nodes)
+        ctx_nodes += [0] * (max_num_ctx_neigh - len(ctx_nodes))
+    else:
+        num_ctx_nodes = max_num_ctx_neigh
+        ctx_nodes = ctx_nodes[:max_num_ctx_neigh]
+
+    return ctx_nodes, num_ctx_nodes
+
+
+def _get_attention_mask(
+    num_ctx_nodes: int, max_num_ctx_neigh: int
+) -> torch.FloatTensor:
+    attention_mask = torch.zeros(max_num_ctx_neigh + 1, dtype=torch.float32)
+    attention_mask[: num_ctx_nodes + 1] = 1
+    return attention_mask
+
+
+def get_input_tensor(
+    graph: nx.Graph,
+    target_node: int,
+    hop_sampling_sizes: List[int],
+    max_num_ctx_neigh: int,
+) -> Tuple[torch.LongTensor, torch.FloatTensor]:
+    ctx_nodes, num_ctx_nodes = _sample_context_neigh(
+        graph, target_node, hop_sampling_sizes, max_num_ctx_neigh
+    )
+
+    assert (
+        len(ctx_nodes) == max_num_ctx_neigh
+    ), f"# of context nodes must be {max_num_ctx_neigh}"
+
+    attention_mask = _get_attention_mask(num_ctx_nodes, max_num_ctx_neigh)
+    return torch.LongTensor([target_node] + ctx_nodes), attention_mask
+
+
 class PMGTDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -39,8 +107,6 @@ class PMGTDataset(torch.utils.data.Dataset):
         self.is_training = is_training
         self.is_inference = is_inference
 
-        self.depth = len(self.hop_sampling_sizes)
-
     def __len__(self) -> int:
         return len(self.node_ids)  # num of nodes
 
@@ -49,7 +115,9 @@ class PMGTDataset(torch.utils.data.Dataset):
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
         target_node = self.node_ids[idx]
 
-        target_inputs = self._get_input_tensor(target_node)
+        target_inputs = get_input_tensor(
+            self.graph, target_node, self.hop_sampling_sizes, self.max_num_ctx_neigh
+        )
 
         if self.is_inference:
             return (target_inputs,)
@@ -60,7 +128,12 @@ class PMGTDataset(torch.utils.data.Dataset):
         neigh_nodes = self._sample_neigh(target_node, num_neigh_nodes)
 
         neigh_input_ids, neigh_attn_mask = zip(
-            *[self._get_input_tensor(n) for n in neigh_nodes]
+            *[
+                get_input_tensor(
+                    self.graph, n, self.hop_sampling_sizes, self.max_num_ctx_neigh
+                )
+                for n in neigh_nodes
+            ]
         )
         neigh_input_ids = torch.stack(neigh_input_ids)
         neigh_attn_mask = torch.stack(neigh_attn_mask)
@@ -73,7 +146,12 @@ class PMGTDataset(torch.utils.data.Dataset):
         neg_nodes = self._sample_neg(target_node, num_neg)
 
         neg_input_ids, neg_attn_mask = zip(
-            *[self._get_input_tensor(n) for n in neg_nodes]
+            *[
+                get_input_tensor(
+                    self.graph, n, self.hop_sampling_sizes, self.max_num_ctx_neigh
+                )
+                for n in neg_nodes
+            ]
         )
         neg_input_ids = torch.stack(neg_input_ids)
         neg_attn_mask = torch.stack(neg_attn_mask)
@@ -85,43 +163,6 @@ class PMGTDataset(torch.utils.data.Dataset):
         )
 
         return target_inputs, pair_inputs, labels
-
-    def _sample_context_neigh(self, target_node: int) -> Tuple[List[int], int]:
-        scores = defaultdict(lambda: 0)
-
-        sampled_nodes = [[target_node]] + [[] for _ in range(self.depth)]
-
-        for k, sample_size in enumerate(self.hop_sampling_sizes, start=1):
-            for node in sampled_nodes[k - 1]:
-                weights = ss.softmax(
-                    np.asarray([v["weight"] for v in self.graph[node].values()])
-                )
-                node_list = np.random.choice(
-                    self.graph[node], size=sample_size, replace=True, p=weights
-                ).tolist()
-                sampled_nodes[k].extend(node_list)
-
-            counter = Counter(sampled_nodes[k])
-
-            for node, freq in counter.items():
-                if node == target_node:
-                    continue
-                scores[node] += freq * (self.depth - k + 1)
-
-        ctx_nodes, _ = zip(
-            *sorted(scores.items(), key=lambda item: item[1], reverse=True)
-        )
-
-        ctx_nodes = list(ctx_nodes)
-
-        if len(ctx_nodes) < self.max_num_ctx_neigh:
-            num_ctx_nodes = len(ctx_nodes)
-            ctx_nodes += [0] * (self.max_num_ctx_neigh - len(ctx_nodes))
-        else:
-            num_ctx_nodes = self.max_num_ctx_neigh
-            ctx_nodes = ctx_nodes[: self.max_num_ctx_neigh]
-
-        return ctx_nodes, num_ctx_nodes
 
     def _sample_neigh(self, target_node: int, max_samples: int) -> List[int]:
         neigh = list(self.graph[target_node].keys())
@@ -137,21 +178,6 @@ class PMGTDataset(torch.utils.data.Dataset):
                 candidate = np.random.randint(len(self.graph)) + 2
             neg_nodes.append(candidate)
         return neg_nodes
-
-    def _get_attention_mask(self, num_ctx_nodes: int) -> torch.Tensor:
-        attention_mask = torch.zeros(self.max_num_ctx_neigh + 1, dtype=torch.float32)
-        attention_mask[: num_ctx_nodes + 1] = 1
-        return attention_mask
-
-    def _get_input_tensor(self, target_node: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        ctx_nodes, num_ctx_nodes = self._sample_context_neigh(target_node)
-
-        assert (
-            len(ctx_nodes) == self.max_num_ctx_neigh
-        ), f"# of context nodes must be {self.max_num_ctx_neigh}"
-
-        attention_mask = self._get_attention_mask(num_ctx_nodes)
-        return torch.LongTensor([target_node] + ctx_nodes), attention_mask
 
     def _get_label_tensor(self, num_pos: int, num_neg: int) -> torch.Tensor:
         return torch.FloatTensor([1] * num_pos + [0] * num_neg)
