3e121f5cb9a66dc2c39ec77be4e6822b9cc6b514
Sangwoo Han <realappsdev@gmail.com>
2022-01-15 12:32:24 +0900

    Update HPO config for NeuMF-MLP

--- scripts/run_hpo_ncf_neumf_end.sh
+++ scripts/run_hpo_ncf_neumf_end.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 args=(
-    --study-name "NCF-NeuMF-end"
+    --study-name "NCF-NeuMF-end-2"
     --train-config-path './config/hpo/train_ncf_neumf_end_params.json'
     --hp-config-path './config/hpo/hpo_ncf_neumf_end_params.yaml'
     --train-name 'ncf'
-    --n-trials 100
+    --n-trials 50
 )
 
 python main.py hp-tuning "${args[@]}"
